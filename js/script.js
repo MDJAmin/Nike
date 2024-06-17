14584d33622dbd80f0d58780a98bcad38b008e9f
MDJAmin <mohammadamindf@gmail.com>
2024-06-17 21:48:23 +0430

    Nike
    add the 3D card

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var modeSwitch = document.querySelector(".mode-switch");
+  
+    modeSwitch.addEventListener("click", function () {
+      document.documentElement.classList.toggle("dark");
+      modeSwitch.classList.toggle("active");
+    });
+  });
+  
+
+  
