a31adf3e8a854f1426871d824ec6815820024943
MDJAmin <mohammadamindf@gmail.com>
2024-06-18 13:12:41 +0430

    Nike
    clean some file

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var modeSwitch = document.querySelector(".mode-switch");
-  
-    modeSwitch.addEventListener("click", function () {
-      document.documentElement.classList.toggle("dark");
-      modeSwitch.classList.toggle("active");
-    });
-  });
-  
+  var modeSwitch = document.querySelector(".mode-switch");
 
-  
+  modeSwitch.addEventListener("click", function () {
+    document.documentElement.classList.toggle("dark");
+    modeSwitch.classList.toggle("active");
+  });
+});
